30baee48fc58f5d3d028cae2524c2a69be070454
Munge Kevin <orokomunge@gmail.com>
2024-12-21 07:38:18 +0300

    :art: adds firestore configs

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  useEmulators: false,
+  
+  firebase: {
+    apiKey: 'AIzaSyAQQWjzVUKV_wCkgom7B_-v6ybefAggT-I',
+    authDomain: 'kurasa-phonebook-e7b4b.firebaseapp.com',
+    projectId: 'kurasa-phonebook-e7b4b',
+    storageBucket: 'kurasa-phonebook-e7b4b.firebasestorage.app',
+    messagingSenderId: '465901750006',
+    appId: '1:465901750006:web:6f516bf28fe6b2c21c0ed7',
+    measurementId: 'G-Q15SGTH9F2',
+  },
+};
