6fdb6f39b9dff9366cfa74063914d3f7a1d6e7ab
Munge Kevin <orokomunge@gmail.com>
2024-12-21 07:39:55 +0300

    :art: entry point

---
+++ libs/elements/page-phonebook/src/lib/components/phonebook-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-app-page',
+  templateUrl: './phonebook-page.component.html',
+  styleUrl: './phonebook-page.component.css',
+})
+export class PhonebookPageComponent {}
