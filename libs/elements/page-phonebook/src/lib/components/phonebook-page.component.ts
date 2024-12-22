74515742bf813e1005688b2c5ab5370890041e92
Munge Kevin <orokomunge@gmail.com>
2024-12-22 20:33:46 +0300

    :fire: removes unused files

--- libs/elements/page-phonebook/src/lib/components/phonebook-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-app-page',
-  templateUrl: './phonebook-page.component.html',
-  styleUrl: './phonebook-page.component.css',
-})
-export class PhonebookPageComponent {}
