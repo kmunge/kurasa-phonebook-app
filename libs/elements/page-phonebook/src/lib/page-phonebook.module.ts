b53fb2d0e27f116b24208696b0a35584ca670822
Munge Kevin <orokomunge@gmail.com>
2024-12-22 20:34:55 +0300

    :art: phone book page dependencies

--- libs/elements/page-phonebook/src/lib/page-phonebook.module.ts
+++ libs/elements/page-phonebook/src/lib/page-phonebook.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PhonebookPageComponent } from './components/phonebook-page.component';
+import { PhonebookPageComponent } from './components/phonebook-page/phonebook-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { PhonebookSidemenuComponent } from './components/phonebook-sidemenu/phonebook-sidemenu.component';
+import { PhonebookHeaderComponent } from './components/phonebook-header/phonebook-header.component';
+import { SidemenuToggleService } from './providers/sidemenu-toggle.service';
+
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [PhonebookPageComponent],
-  exports: [PhonebookPageComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+  ],
+  declarations: [
+    PhonebookPageComponent,
+    PhonebookSidemenuComponent,
+    PhonebookHeaderComponent,
+  ],
+  exports: [
+    PhonebookPageComponent,
+    PhonebookSidemenuComponent,
+    PhonebookHeaderComponent,
+  ],
+  providers: [SidemenuToggleService],
 })
 export class PagePhonebookModule {}
