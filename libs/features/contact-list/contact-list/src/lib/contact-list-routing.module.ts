28099dfaa00a89e038f673f093798ce0580d3966
Munge Kevin <orokomunge@gmail.com>
2024-12-23 09:55:31 +0300

    :lipstick: fixes the routing for contact details

--- libs/features/contact-list/contact-list/src/lib/contact-list-routing.module.ts
+++ libs/features/contact-list/contact-list/src/lib/contact-list-routing.module.ts
@@ -3,7 +3,18 @@ import { Route, RouterModule } from '@angular/router';
 import { ContactListComponent } from './components/contact-list/contact-list.component';
 
 const HOME_ROUTES: Route[] = [
-  { path: '', component: ContactListComponent, data: { breadCrumb: '' } },
+  { path: '', 
+    component: ContactListComponent, data: { breadCrumb: '' }, 
+    children: [
+        {
+            path: 'details',
+            loadChildren: () =>
+              import('libs/features/contact-details/src/lib/contact-details.module').then(
+                (m) => m.ContactDetailsModule
+              ),
+        },
+    ],
+},
 ];
 
 @NgModule({
--- libs/features/contact-list/contact-list/src/lib/contact-list.module.ts
+++ libs/features/contact-list/contact-list/src/lib/contact-list.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ContactListComponent } from './components/contact-list/contact-list.component';
 import { ContactListRouterModule } from './contact-list-routing.module';
 import { PagePhonebookModule } from 'page-phonebook';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     ContactListRouterModule,
     PagePhonebookModule,
   ],
