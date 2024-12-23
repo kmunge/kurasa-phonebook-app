5c3030082b34ba2b867659d3935c20b57a544ee0
Munge Kevin <orokomunge@gmail.com>
2024-12-23 09:56:11 +0300

    :art: adds get by and update contact

--- libs/state/services/contact.service.ts
+++ libs/state/services/contact.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Contact } from 'libs/models/contact.model';
-import { tap } from 'rxjs/operators';
+import { from, Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class ContactsService {
   private collectionName = 'contacts';
-
-  constructor(private firestore: AngularFirestore) {}
+  constructor(private firestore: AngularFirestore, private http: HttpClient) {}
 
   addContact(contact: Contact): Promise<void> {
     const id = this.firestore.createId();
@@ -29,4 +29,23 @@ export class ContactsService {
       )
       .valueChanges();
   }  
+
+  getContactById(id: string) {
+    return this.firestore
+      .collection<Contact>(this.collectionName)
+      .doc(id)
+      .valueChanges();
+  }
+
+  updateContact(contact: Contact): Observable<void> {
+    return from(
+      this.firestore
+        .collection(this.collectionName)
+        .doc(contact.id)
+        .update({ ...contact })
+    );
+  }
+  
+  
 }
+
