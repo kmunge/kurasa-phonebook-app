9953a83d35b7715a75f48321f4b4c5d5b36202a8
Munge Kevin <orokomunge@gmail.com>
2024-12-28 12:21:22 +0300

    :lipstick: moves to add contacts lib

---
+++ libs/features/add-contacts/src/lib/service/contact.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { HttpClient } from '@angular/common/http';
+
+import { from, Observable } from 'rxjs';
+import { Contact } from '../models/contact.model';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ContactsService {
+  private collectionName = 'contacts';
+  constructor(private firestore: AngularFirestore, private http: HttpClient) {}
+
+  addContact(contact: Contact): Promise<void> {
+    const id = this.firestore.createId();
+    return this.firestore
+      .collection(this.collectionName)
+      .doc(id)
+      .set({ ...contact, id, isDeleted: false, isFavorite: false });
+  }
+
+  getContacts(): Observable<Contact[]> {
+    return this.firestore
+      .collection<Contact>(this.collectionName, ref =>
+        ref
+          .where('isDeleted', '==', false) // Fetch only non-deleted contacts
+          .orderBy('firstName', 'asc') // Sort alphabetically by firstName
+      )
+      .valueChanges();
+  }  
+
+  getContactById(id: string) {
+    return this.firestore
+      .collection<Contact>(this.collectionName)
+      .doc(id)
+      .valueChanges();
+  }
+
+  updateContact(contact: Contact): Observable<void> {
+    return from(
+      this.firestore
+        .collection(this.collectionName)
+        .doc(contact.id)
+        .update({ ...contact })
+    );
+  }
+ 
+}
+
